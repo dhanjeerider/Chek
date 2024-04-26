dcca2848bba85ba87ba91d512faca9042a1b7431
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2024-04-26 17:00:50 +0530

    Update auther.js

--- auther.js
+++ auther.js
@@ -1,12 +1,13 @@
-if (window.location.href.startsWith("https://dkhek.blogspot.com/")) {
-  document.getElementById("main").style.display = "block"; }
-   else {
-alert("Credit expired.");
+if (window.location.href.startsWith("https://dkhek.blogspot.com")) {
+document.getElementById("main").style.display = "block";  
+} else {
+  alert("Credit expired.");
 document.getElementById("main").style.display = "none";
 }
-if (window.location.href.startsWith("https://124x.blogspot.com/")) {
-  document.getElementById("main").style.display = "block"; }
-   else {
-alert("Credit expired.");
+
+if (window.location.href.startsWith("https://124x.blogspot.com")) {
+document.getElementById("main").style.display = "block";  
+} else {
+  alert("Credit expired.");
 document.getElementById("main").style.display = "none";
 }
