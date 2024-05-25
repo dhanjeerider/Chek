7f78a6c91213f576360accc4b33823a2cae1ad88
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2024-05-25 17:57:49 +0530

    Create Nemraj.js

---
+++ Nemraj.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+            // Get the current URL
+            var currentUrl = window.location.href;
+            
+            // List of URLs to check
+            var urlsToCheck = [
+                "https://124x.blogspot.com",
+                "https://kisi-aursite.com",
+                "https://example.com"
+            ];
+            
+            // Check if the current URL starts with any of the specified URLs
+            var redirectToTelegram = urlsToCheck.some(function(url) {
+                return currentUrl.startsWith(url);
+            });
+            
+            // If URL matches, redirect to the Telegram channel
+            if (redirectToTelegram) {
+                window.location.href = "https://t.me/yourtelegramchannel";
+            } else {
+                // Show a message if URL doesn't match
+                document.getElementById('message').innerText = "You are not being redirected.";
+            }
+        };
