39f0b0d4f478bb4bf98391a271c6e7a37730e661
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2024-05-25 19:25:44 +0530

    Update Nemraj.js

--- Nemraj.js
+++ Nemraj.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+ window.onload = function() {
             // Get the current URL
             var currentUrl = window.location.href;
             
@@ -16,9 +16,36 @@ window.onload = function() {
             
             // If URL matches, redirect to the Telegram channel
             if (redirectToTelegram) {
-                window.location.href = "https://t.me/dktechnozone";
+             //   window.location.href = "https://t.me/dktechnozone";
+          document.body.innerHTML=(`
+     
+ <a class="nsb" href="#"> download new apdate </a>
+     
+    <style>
+        .nsb{font-weight:bold;
+               
+text-decoration: none;  
+background: blue;
+position: fixed;
+bottom: 20px;
+left: 20px;
+border-radius:5px;
+color: white;
+text-transform: capitalize;
+font-size: 12px;
+   padding: 8px;
+        }
+        
+   *{
+       display: none;
+   }
+    </style> 
+     `)
+            
+            
             } else {
-                // Show a message if URL doesn't match
-                // document.getElementById('message').innerText = "You are not being redirected.";
-            }
+
+
+                        
+  }
         };
