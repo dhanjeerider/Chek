dbed224cb26405f04ea5c8537b2e0fdd33be3d21
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2024-05-25 17:15:21 +0530

    Update dkfile.js

--- dkfile.js
+++ dkfile.js
@@ -1,25 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-        // Array of URLs to target
-        var targetUrls = [
-            'https://ghdjududuxbxbxb.blogspot.com/',
-            'https://ghdjududuxbxbxb.blogspot.com',
-            'https://example3.blogspot.com'
-        ];
 
-        // Get the current URL
-        var currentUrl = window.location.href;
-
-        // Check if the current URL matches any in the targetUrls array
-        var isTargetUrl = targetUrls.some(function(url) {
-            return currentUrl.startsWith(url);
-        });
-
-        // If it matches, replace the body content
-        if (isTargetUrl) {
-            document.body.innerHTML = '';
-        
-        }
-    });
-
-console.log("theme by dktechnozone")
+console.log("theme by dktechnozone.in")
 
