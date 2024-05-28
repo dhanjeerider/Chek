e871bc767d7486221cbe6da00a022485b6fea564
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2024-05-28 16:48:13 +0530

    Update icon.js

--- icon.js
+++ icon.js
@@ -1,34 +1,6 @@
-var allowedURLs = [
-    "https://dkhek.blogspot.com",
-    "https://draft.blogger.com",
-    "https://www.blogger.com",
-    "https://newyearevenings.blogspot.com/",
-"https://vega-clone.blogspot.com/",
-"https://dkchek1.blogspot.com/",
-"https://girgit-ui.blogspot.com/",
-"https://apk-store-4u.blogspot.com/"
-    "https://www.filmyslap.xyz/",   
-   "https://fletrothemes.blogspot.com/", 
-    "https://faddalunga.blogspot.com/",
-    "https://124x.blogspot.com",
-    "https://www.myanimehindi.in",
-    "https://jet-movie.blogspot.com",
-    "https://testyyyyrr4.blogspot.com"
-];
+var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyanalisis||0!=document.body.scrollTop&&!1===lazyanalisis)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyanalisis=!0)},!0);
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag(&#39;js&#39;, new Date());
 
-var foundMatch = false;
-
-for (var i = 0; i < allowedURLs.length; i++) {
-    if (window.location.href.startsWith(allowedURLs[i])) {
-        foundMatch = true;
-        break;
-    }
-}
-if (foundMatch) {
-    document.getElementById("main").style.display = "block";
-} else {
-    setTimeout(function() {
-        document.body.innerHTML = "Your website is not verified to use this theme. Just send your site URL to admin at https://t.me/dhanjeerider to verify your domain.";
-    }, 5000); 
-    document.getElementById("main").style.display = "none";
-}
+  gtag(&#39;config&#39;, &#39;G-2VYRMPXK0F&#39;);
