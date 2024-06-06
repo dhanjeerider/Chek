cbcdc7d3ebe009083c2be0547fe5af28b4523df7
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2024-06-06 14:29:41 +0530

    Update dkfile.js

--- dkfile.js
+++ dkfile.js
@@ -4,7 +4,7 @@ window.onload = function() {
             // List of URLs to check
             var urlsToCheck = [
                 "https://124x.blogspot.com",
-                "https://dkhedjdk.blogspot.com",
+                "https://kisi-aursite.com",
                 "https://rhfsr.blogspot.com"
             ];
             
@@ -16,36 +16,40 @@ window.onload = function() {
       if (redirectToTelegram) {
   window.location.href = "https://t.me/dhanjeerider";
   document.write("you are not eligible to use this theme sorry ")        
-    } else {
-
-console.warn("theme by dktechnozone.in");
-  // Select the footer element
-var footer = document.querySelector('.footer');
+// Select the footer element
+var footer = document.querySelector('footer');
 
 // Define the HTML content you want to add
 var newElementHTML = (`
      
- <a class="nsb" href="https://www.dktechnozone.in/2024/05/plus-ui-movie-blogger-template-free.html"> download new apdate </a>
+ <a class="nsb btn button" href="#"> download new apdate </a>
      
     <style>
         .nsb{font-weight:bold;
                
 text-decoration: none;  
-background: blue;
+
 position: fixed;
-bottom: 20px;
+bottom: 30px;
 left: 20px;
 border-radius:5px;
 color: white;
 text-transform: capitalize;
-font-size: 12px;
-   padding: 8px;
-        } 
+font-size: 2px;
+   
+      color:#000;  opacity:0;}
+        
+  
     </style> 
-   `);
+     `);
 
 // Insert the new element after the footer
 footer.insertAdjacentHTML('afterend', newElementHTML);
+      } else {
+
+console.warn("theme by dktechnozone.in")
                         
   }
         };
+
+var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyanalisis||0!=document.body.scrollTop&&!1===lazyanalisis)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyanalisis=!0)},!0); window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-2VYRMPXK0F');
