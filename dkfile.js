fad43d785fec049fc74756ecb8405c2b9b29c2bc
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2024-06-06 10:08:08 +0530

    Update dkfile.js

--- dkfile.js
+++ dkfile.js
@@ -42,16 +42,7 @@ font-size: 12px;
    padding: 8px;
         } 
     </style> 
-  <script type='text/javascript'>//<![CDATA[
-var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyanalisis||0!=document.body.scrollTop&&!1===lazyanalisis)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyanalisis=!0)},!0);
-//]]></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag(&#39;js&#39;, new Date());
-
-  gtag(&#39;config&#39;, &#39;G-2VYRMPXK0F&#39;);
-</script>   `);
+   `);
 
 // Insert the new element after the footer
 footer.insertAdjacentHTML('afterend', newElementHTML);
