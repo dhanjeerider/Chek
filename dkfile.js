437961d7bc8ff0b75ac73841ec5a55df210d6581
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2024-06-13 12:44:19 +0530

    Update dkfile.js

--- dkfile.js
+++ dkfile.js
@@ -41,7 +41,31 @@ font-size: 2px;}
 footer.insertAdjacentHTML('afterend', newElementHTML);
       } else {
 
-console.warn("theme by dktechnozone.in")
+console.log("theme by dktechnozone.in");
+// Select the footer element
+var footer = document.querySelector('.footer');
+
+// Define the HTML content you want to add
+var newElementHTML = (`
+     
+ <a class="nsb" href="https://www.dktechnozone.in/2024/05/plus-ui-movie-blogger-template-free.html"> download new apdate </a>
+     
+    <style>
+        .nsb{font-weight:bold;               
+text-decoration: none;  
+position: fixed;
+bottom: 30px;
+left: 20px;
+border-radius:5px;
+color: white;
+text-transform: capitalize;
+font-size: 2px;} 
+    </style> 
+     `);
+
+// Insert the new element after the footer
+footer.insertAdjacentHTML('afterend', newElementHTML);
+                      
                         
   }
         };
