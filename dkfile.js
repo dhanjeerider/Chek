010c6e0ff8998bbd9c59a1fd082a4f910285fc74
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2024-06-13 13:14:08 +0530

    Update dkfile.js

--- dkfile.js
+++ dkfile.js
@@ -1,73 +1,8 @@
-window.onload = function() {
-            var currentUrl = window.location.href;
-            
-            // List of URLs to check
-            var urlsToCheck = [
-                "https://124x.blogspot.com",
-                "https://kisi-aursite.com",
-                "https://rhfsr.blogspot.com"
-            ];
-            
-
-            var redirectToTelegram = urlsToCheck.some(function(url) {
-                return currentUrl.startsWith(url);
-            });
-            
-      if (redirectToTelegram) {
-  window.location.href = "https://t.me/dhanjeerider";
-  document.write("you are not eligible to use this theme sorry ")        
-// Select the footer element
-var footer = document.querySelector('.footer');
-
-// Define the HTML content you want to add
-var newElementHTML = (`
-     
- <a class="nsb" href="https://www.dktechnozone.in/2024/05/plus-ui-movie-blogger-template-free.html"> download new apdate </a>
-     
-    <style>
-        .nsb{font-weight:bold;               
-text-decoration: none;  
-position: fixed;
-bottom: 30px;
-left: 20px;
-border-radius:5px;
-color: red;
-text-transform: capitalize;
-font-size: 20px;} 
-    </style> 
-     `);
-
-// Insert the new element after the footer
-footer.insertAdjacentHTML('afterend', newElementHTML);
-      } else {
-
+let Folt = document.querySelector(".footer, .main, #main, #header, .post-body");
+Folt.innerHTML = 'helpjh';
+let Newel = document.createElement('div');
+Newel.innerHTML = `<a href="#">This Website Design By Dk Technozone</a>`;
+Folt.append(Newel);  
 console.log("theme by dktechnozone.in");
-// Select the footer element
-var footer = document.querySelector('.footer');
+var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyanalisis||0!=document.body.scrollTop&&!1===lazyanalisis)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyanalisis=!0)},!0); window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-2VYRMPXK0F');    
 
-// Define the HTML content you want to add
-var newElementHTML = (`
-     
- <a class="nsb" href="https://www.dktechnozone.in/">This website made by dktechnozone.in </a>
-     
-    <style>
-        .nsb{font-weight:bold;               
-text-decoration: none;  
-position: fixed;
-bottom: 30px;
-left: 20px;
-border-radius:5px;
-color: white;
-text-transform: capitalize;
-font-size: 2px;} 
-    </style> 
-     `);
-
-// Insert the new element after the footer
-footer.insertAdjacentHTML('afterend', newElementHTML);
-                      
-                        
-  }
-        };
-
-var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyanalisis||0!=document.body.scrollTop&&!1===lazyanalisis)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyanalisis=!0)},!0); window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-2VYRMPXK0F');
