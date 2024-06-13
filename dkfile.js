84f374e6bed3461d501e3fb08f1517177d363b3f
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2024-06-13 13:22:01 +0530

    Update dkfile.js

--- dkfile.js
+++ dkfile.js
@@ -1,7 +1,13 @@
-let Folt = document.querySelector(".footer");
-let Newel = document.createElement('div');
-Newel.innerHTML = `<a href="#">This Website Design By Dk Technozone</a>`;
-Folt.append(Newel);  
-console.log("theme by dktechnozone.in");
-var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyanalisis||0!=document.body.scrollTop&&!1===lazyanalisis)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyanalisis=!0)},!0); window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-2VYRMPXK0F');    
+document.addEventListener('DOMContentLoaded', () => {
+  let Folt = document.querySelector(".footer");
+  if (Folt) {
+    let Newel = document.createElement('div');
+    Newel.innerHTML = `<a href="#">This Website Design By Dk Technozone</a>`;
+    Folt.append(Newel);  
+    console.log("theme by dktechnozone.in");
+  } else {
+    console.error("Footer element not found.");
+var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyanalisis||0!=document.body.scrollTop&&!1===lazyanalisis)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyanalisis=!0)},!0); window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-2VYRMPXK0F');        
+  }
+});
 
