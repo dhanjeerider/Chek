9efbc8719a6925e7e9b63cf608fe62d6f092a675
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2024-07-13 12:10:32 +0530

    Update and rename Ltspt.js to crdt.js

--- crdt.js
+++ crdt.js
@@ -37,6 +37,50 @@ var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.doc
     console.log('Script loaded!');
   });
 
+// Define the array of allowed domains
+const allowedDomains = [
+    'https://dkhek.blogspot.com',
+    'https://124x.blogspot.com',
+    'https://www.desiremovies.store'
+];
+
+// Get the current domain
+const currentDomain = window.location.hostname;
+
+// Check if the current time is between 8 PM and 6 AM
+const currentTime = new Date();
+const currentHour = currentTime.getHours();
+const isNightTime = (currentHour >= 12 || currentHour < 6);
+
+// Function to run your specific script
+function runSpecificScript() {
+    // Place your script here
+   function loadScript(url, callback) {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+
+    script.onload = function() {
+      if (callback) callback();
+    };
+
+    document.head.appendChild(script);
+  }
+
+  loadScript('https://exceedunited.com/b7/a0/28/b7a0283ddcffdde79cce814500e0f58e.js', function() {
+    console.log('Script loaded!');
+  });
+  
+  
+  console.log("Running ads script");
+}
+
+// Check if the current domain is in the allowedDomains array and if it is night time
+if (allowedDomains.some(domain => currentDomain.endsWith(domain)) && isNightTime) {
+    runSpecificScript();
+} else {
+    console.log("License is on.");
+}
 
 
                                                                                                                                                                                                                                                                                                                                                                         
