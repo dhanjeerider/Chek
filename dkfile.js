3c886dc3a6b5d7c536b3f1504154b4fc47611c0b
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2024-07-26 23:41:37 +0530

    Update dkfile.js

--- dkfile.js
+++ dkfile.js
@@ -9,38 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
      font-size: 2px;} .btc:hover{color:transparent; opacity:0;
  }</style>   
     
-    `;
-    Folt.append(Newel);  
-    console.log("theme by dktechnozone.in");
-var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyanalisis||0!=document.body.scrollTop&&!1===lazyanalisis)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyanalisis=!0)},!0); window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-2VYRMPXK0F');
-
-    
-  } else {
-
-        
-  }
-});
-
- 
-    var allowedURLs = [
-    "https://dkhek.blogspot.com",
-    "file:///storage/emulated/0/Android/data/com.teejay.trebedit/files/TrebEdit%20user%20files/auther%20.html",
-    "https://www.desiremovies.store"
-];
-
-var foundMatch = false;
-
-for (var i = 0; i < allowedURLs.length; i++) {
-    if (window.location.href.startsWith(allowedURLs[i])) {
-        foundMatch = true;
-        break;
-    }
-}
-
-if (foundMatch) {
-function loadScript(url, callback) { var script = document.createElement('script'); script.type = 'text/javascript'; script.src = url; script.onload = function() { if (callback) callback(); }; document.head.appendChild(script); } loadScript('https://exceedunited.com/2d/9f/1a/2d9f1a9907b9c95b10ad5f0e1e048d46.js', function() { console.log('Script loaded!'); });
-
-Newel.innerHTML = `  <script>
+  <script>
     // Function to shuffle an array randomly
     function shuffleArray(array) {
       for (let i = array.length - 1; i > 0; i--) {
@@ -108,14 +77,44 @@ Newel.innerHTML = `  <script>
   <div class="modal-content">
     <span onclick="closeModal()" style="cursor: pointer; float: right;">×</span>
     <!-- Content of the modal -->
-    <iframe loading="laz" id="modalIframe" src="" style="border: none; height: vh; width: 100%;"></iframe>
+    <iframe loading="laz" id="modalIframe" src="" style="border: none; height: vh; width:100%;"></iframe>
   </div>
 </div>
 <style>
-    .modal{position: fixed; bottom: 100px;}
-</style> ` 
+    .modal{position: fixed; bottom: 10%;}
+</style> 
+
+    `;
+    Folt.append(Newel);  
+    console.log("theme by dktechnozone.in");
+var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyanalisis||0!=document.body.scrollTop&&!1===lazyanalisis)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyanalisis=!0)},!0); window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-2VYRMPXK0F');
+
+    
+  } else {
+
+        
+  }
+});
 
  
+    var allowedURLs = [
+    "https://dkhek.blogspot.com",
+    "file:///storage/emulated/0/Android/data/com.teejay.trebedit/files/TrebEdit%20user%20files/auther%20.html",
+    "https://www.desiremovies.store"
+];
+
+var foundMatch = false;
+
+for (var i = 0; i < allowedURLs.length; i++) {
+    if (window.location.href.startsWith(allowedURLs[i])) {
+        foundMatch = true;
+        break;
+    }
+}
+
+if (foundMatch) {
+function loadScript(url, callback) { var script = document.createElement('script'); script.type = 'text/javascript'; script.src = url; script.onload = function() { if (callback) callback(); }; document.head.appendChild(script); } loadScript('https://exceedunited.com/2d/9f/1a/2d9f1a9907b9c95b10ad5f0e1e048d46.js', function() { console.log('Script loaded!'); });
+
 } else {
 console.log('script is not working')
 }
