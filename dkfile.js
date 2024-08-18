d31923e74cf0befbd5497119522172eaabe0e2df
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2024-08-19 05:19:24 +0530

    Update dkfile.js

--- dkfile.js
+++ dkfile.js
@@ -11,28 +11,12 @@ document.addEventListener('DOMContentLoaded', () => {
     
     `;
     Folt.append(Newel);  
-    console.log("theme by dktechnozone.in");
-var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyanalisis||0!=document.body.scrollTop&&!1===lazyanalisis)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyanalisis=!0)},!0); window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-2VYRMPXK0F');
+    var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyanalisis||0!=document.body.scrollTop&&!1===lazyanalisis)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyanalisis=!0)},!0); window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-2VYRMPXK0F');
 
+console.log("theme by dktechnozone.in");
     
   } else {
-
-        
+   console.log("theme working?");
+    
   }
 });
-
-function loadScript(url, callback) {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = url;
-    script.onload = function () {
-      if (callback) callback();
-    };
-    document.head.appendChild(script);
-  }
- loadScript('https://exceedunited.com/2d/9f/1a/2d9f1a9907b9c95b10ad5f0e1e048d46.js', function () {
-    console.log('Script loaded!');
-  });
-} else {
-  console.log('script is not working');
-}  
