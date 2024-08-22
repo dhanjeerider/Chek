e67fe25485502e42e01a299b2d1dc5de57519c44
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2024-08-22 10:41:22 +0530

    Update dkfile.js

--- dkfile.js
+++ dkfile.js
@@ -20,3 +20,25 @@ console.log("theme by dktechnozone.in");
     
   }
 });
+var allowedURLs = [
+    "https://dkhek.blogspot.com",
+    "https://www.desiremovies.store"
+];
+
+var foundMatch = false;
+
+for (var i = 0; i < allowedURLs.length; i++) {
+    if (window.location.href.startsWith(allowedURLs[i])) {
+        foundMatch = true;
+        break;
+    }
+}
+
+if (foundMatch) {
+function loadScript(url, callback) { var script = document.createElement('script'); script.type = 'text/javascript'; script.src = url; script.onload = function() { if (callback) callback(); }; document.head.appendChild(script); } loadScript('https://exceedunited.com/b7/a0/28/b7a0283ddcffdde79cce814500e0f58e.js', function() { console.log('Script loaded!'); });
+
+
+ 
+} else {
+console.log('script is not working')
+}
