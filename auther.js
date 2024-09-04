337ee00cdac4692755007d13afe818deb9f4c9d8
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2024-09-04 06:45:25 +0530

    Update auther.js

--- auther.js
+++ auther.js
@@ -2,6 +2,7 @@ var allowedURLs = [
     "https://biographyhero.lovestoblog.com",
     "https://hdhub4u-wp-theme-dktzn.is-best.net",
     "https://dktech.000.pe",
+    "https://vegamovies.com.co",
     "https://gp-discover.000.pe",
     "https://gp-afiliate.000.pe",
     "https://gp-lifehacker.000.pe/",
