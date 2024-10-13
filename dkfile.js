299343082c0cbf4f8989ba032de210012cdccc5d
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2024-10-13 21:49:39 +0530

    Update dkfile.js

--- dkfile.js
+++ dkfile.js
@@ -34,5 +34,5 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         window.onload = function() {
             goToRandomPost();
-            setInterval(goToRandomPost,  5000);
+            setInterval(goToRandomPost,  50000);
         };
