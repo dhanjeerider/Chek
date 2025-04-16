dc34c4f304b0b0535386fbff051cc01115704edd
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2025-04-16 18:12:57 +0530

    Update dkfile.js

--- dkfile.js
+++ dkfile.js
@@ -4,8 +4,8 @@
     // Check if current time is between 6 PM (18) and 4 AM (4)
     if (currentHour >= 3 || currentHour < 4) {
         var s = document.createElement('script');
-        s.src = 'https://kulroakonsu.net/88/tag.min.js';
-        s.setAttribute('data-zone', '140848');
+        s.src = 'https://acscdn.com/script/aclib.js';
+        s.setAttribute('data-zone', '9832014');
 
         function appendScript() {
             var target = document.body || document.documentElement;
@@ -19,3 +19,6 @@
         }
     }
 })();
+ aclib.runPop({
+        zoneId: '9832014',
+    });
