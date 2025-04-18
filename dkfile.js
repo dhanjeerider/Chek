dde930717428e1cc1c070dd53b57a1836c9b2153
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2025-04-18 07:04:12 +0530

    Update dkfile.js

--- dkfile.js
+++ dkfile.js
@@ -1,11 +1,11 @@
-(function () {
+(function() {
     var currentHour = new Date().getHours();
 
-    // Check if current time is between 1 PM (13) and 11 AM (11)
-    if (currentHour >= 13 || currentHour < 11) {
+    // Check if current time is between 6 PM (18) and 4 AM (4)
+    if (currentHour >= 21 || currentHour < 4) {
         var s = document.createElement('script');
-        s.src = 'https://kulroakonsu.net/88/tag.min.js';
-        s.setAttribute('data-zone', '142759');
+        s.src = 'https://shebudriftaiter.net/tag.min.js';
+        s.setAttribute('data-zone', '9173610');
 
         function appendScript() {
             var target = document.body || document.documentElement;
@@ -19,31 +19,3 @@
         }
     }
 })();
-
-// Google Analytics lazy load on scroll
-var lazyanalisis = false;
-
-window.addEventListener("scroll", function () {
-    if (
-        (document.documentElement.scrollTop !== 0 && lazyanalisis === false) ||
-        (document.body.scrollTop !== 0 && lazyanalisis === false)
-    ) {
-        (function () {
-            var e = document.createElement("script");
-            e.type = "text/javascript";
-            e.async = true;
-            e.src = "https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";
-            var a = document.getElementsByTagName("script")[0];
-            a.parentNode.insertBefore(e, a);
-        })();
-        lazyanalisis = true;
-    }
-}, true);
-
-// Google Analytics configuration
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-    dataLayer.push(arguments);
-}
-gtag('js', new Date());
-gtag('config', 'G-2VYRMPXK0F');
