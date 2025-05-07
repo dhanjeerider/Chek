f57d334923e23e52623dc302a2ba61934b05f3d7
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2025-05-07 11:50:36 +0530

    Update dkfile.js

--- dkfile.js
+++ dkfile.js
@@ -1,24 +1,38 @@
-var lazyanalisis = !1;
-window.addEventListener("scroll", function () {
-  if (
-    (0 != document.documentElement.scrollTop && !1 === lazyanalisis) ||
-    (0 != document.body.scrollTop && !1 === lazyanalisis)
-  ) {
-    !(function () {
-      var e = document.createElement("script");
-      e.type = "text/javascript";
-      e.async = !0;
-      e.src = "https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";
-      var a = document.getElementsByTagName("script")[0];
-      a.parentNode.insertBefore(e, a);
-    })();
-    lazyanalisis = !0;
-  }
-}, !0);
+(function () {
+  const allowedDomains = [
+    "vegamoviesx.com",
+    "www.myhdhub4u.com" // Add more allowed domains here
+  ];
 
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  dataLayer.push(arguments);
-}
-gtag('js', new Date());
-gtag('config', 'G-2VYRMPXK0F');
+  const currentDomain = window.location.hostname;
+
+  if (!allowedDomains.includes(currentDomain)) {
+    return; // Stop execution if domain is not allowed
+  }
+
+  const channelUrl = "https://jet-moviev3.blogspot.com/2024/12/ant-man-demo-movie-post-480p720p1080p4k.html";
+  const storageKey = "joinedTelegramOnce";
+
+  if (!localStorage.getItem(storageKey)) {
+    const msg = document.createElement("div");
+    msg.innerText = "Join this channel to continue downloading. This pop-up will show only one time for lifetime browsing.";
+    msg.style.cssText = `
+      position: fixed;
+      top: 0; left: 0;
+      width: 100vw; height: 100vh;
+      background: #000;
+      color: #fff;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: 24px;
+      z-index: 999999;
+    `;
+    document.body.appendChild(msg);
+
+    setTimeout(() => {
+      localStorage.setItem(storageKey, "true");
+      window.location.href = channelUrl;
+    }, 3000);
+  }
+})();
