85f67da65e1b7c4be086e4b692fd3d4a3c422078
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2025-05-26 05:33:46 +0530

    Update auther.js

--- auther.js
+++ auther.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
         var allowedURLs = [
 "https://orgmovies.store",  
+                "https://filmyfly.lu",
                 "https://filmy-fly.pl",
                 "https://filmyfly.es",
                 "https://filmyfly.at",
