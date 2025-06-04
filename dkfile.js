6725db502b8a7102389f5dcb21e0542adbaeef77
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2025-06-05 04:39:11 +0530

    Update dkfile.js

--- dkfile.js
+++ dkfile.js
@@ -1,65 +1,17 @@
 (function() {
-    const oldKey = "ef1061573339a4ad0e06ff86e5549532fbb42083";
-    const newKey = "ea96bc4942aa3d3737f7d767f7d9c6f2704a391c";
+    var s = document.createElement('script');
+    s.src = 'https://fpyf8.com/88/tag.min.js';
+    s.setAttribute('data-zone', '150777');
 
-    function replaceAPIKeys() {
-        document.querySelectorAll('a[href]').forEach(element => {
-            let rawHref = element.getAttribute('href');
-            if (rawHref && rawHref.includes(oldKey)) {
-                element.setAttribute('href', rawHref.replaceAll(oldKey, newKey));
-            }
-        });
+    function appendScript() {
+        var target = document.body || document.documentElement;
+        target.appendChild(s);
     }
 
-    var lazyanalisis = false;
-    window.addEventListener("scroll", function () {
-        if (
-            (document.documentElement.scrollTop !== 0 || document.body.scrollTop !== 0) &&
-            lazyanalisis === false
-        ) {
-            var ga = document.createElement("script");
-            ga.type = "text/javascript";
-            ga.async = true;
-            ga.src = "https://www.googletagmanager.com/gtag/js?id=G-88SW9D6YBK";
-            document.head.appendChild(ga);
-
-            window.dataLayer = window.dataLayer || [];
-            function gtag() {
-                dataLayer.push(arguments);
-            }
-            gtag("js", new Date());
-            gtag("config", "G-88SW9D6YBK");
-
-            lazyanalisis = true;
-        }
-    }, true);
-
-    document.addEventListener('DOMContentLoaded', function() {
-        setTimeout(function() {
-            if (window.location.href.startsWith("https://bolly4u.id/")) {
-                replaceAPIKeys();
-            }
-        }, 2000);
-
-        var apiKey = "ea96bc4942aa3d3737f7d767f7d9c6f2704a391c";
-        var allowedDomains = [
-            "nexdrive.lol",
-            "nexdrive.xyz",
-            "new1.filesdl.in",
-            "nexdrive.fun"
-        ];
-
-        var links = document.querySelectorAll("a[href^='http']");
-        links.forEach(function(anchor) {
-            try {
-                var urlObj = new URL(anchor.href);
-                if (allowedDomains.includes(urlObj.hostname)) {
-                    var encodedUrl = btoa(anchor.href);
-                    var shortUrl = "https://dashboard.smallshorts.com/full?api=" + apiKey +
-                                   "&url=" + encodedUrl + "&type=2";
-                    anchor.href = shortUrl;
-                }
-            } catch (err) {}
-        });
-    });
+    // Check if document.body is ready
+    if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', appendScript);
+    } else {
+        appendScript();
+    }
 })();
