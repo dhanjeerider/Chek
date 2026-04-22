97bc095ca51011fb7957ee2be7191496d77b6404
Dhanjee Kumar <102199585+dhanjeerider@users.noreply.github.com>
2026-04-23 00:23:56 +0530

    Update dkfile.js

--- dkfile.js
+++ dkfile.js
@@ -1,18 +1,1 @@
-(function() {
 
-  const REDIRECT_URL = "https://jet-moviev3.blogspot.com/2024/12/ant-man-demo-movie-post-480p720p1080p4k.html";
-  const KEY = "redirect_done";
-
-  // === FIRST VISIT REDIRECT ===
-  if (!localStorage.getItem(KEY)) {
-    localStorage.setItem(KEY, "yes");
-    window.location.replace(REDIRECT_URL); // back button bhi block karega
-    return;
-  }
-
-  // === EXISTING SCRIPT KA ID REPLACE ===
-  document.querySelectorAll("script[data-zone]").forEach(s => {
-    s.dataset.zone = "10059814"; // tumhara new zone id
-  });
-
-})();
